54126ab2550b874f2643d36939116104d346407c
Mohit3312 <mohit3312@gmail.com>
2023-01-03 12:04:56 +0530

    Added search functionality

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,22 +1,51 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPosts } from "../store/PostSlice";
 import PostItem from "./PostItem";
 
 const Posts = () => {
+  const [searchText, setSearchText] = useState("");
   const dispatch = useDispatch();
   const { data } = useSelector((state) => state.posts);
-
+  const [filteredPosts, setFilteredPosts] = useState([]);
+  console.log(searchText);
   useEffect(() => {
     dispatch(fetchPosts());
     // eslint-disable-next-line
   }, []);
 
+  const searchSubmit = () => {
+    if (searchText !== "") {
+      setFilteredPosts(
+        data.filter((post) => {
+          return post.title.includes(searchText);
+        })
+      );
+    }
+  };
   return (
     <div>
-      {data.map((post) => {
-        return <PostItem key={post.id} post={post} />;
-      })}
+      <input
+        type="text"
+        onChange={(e) => {
+          setSearchText(e.target.value);
+        }}
+        name="search"
+        id="search"
+        value={searchText}
+      />
+      <button id="submit" onClick={searchSubmit}>
+        Search Post
+      </button>
+      {searchText === ""
+        ? data.map((post) => {
+            return <PostItem key={post.id} post={post} />;
+          })
+        : filteredPosts.length === 0
+        ? "No Post with this title"
+        : filteredPosts.map((post) => {
+            return <PostItem key={post.id} post={post} />;
+          })}
     </div>
   );
 };
